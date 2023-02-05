9497e9f0d7dac92a404ac21bc2512e7135159714
Sattwik Basu <sattwikbasu@Sattwiks-MacBook-Pro.local>
2023-02-05 02:59:08 -0600

    bit of optimization in the main py file

--- HW1/code/hw1.py
+++ HW1/code/hw1.py
@@ -22,7 +22,7 @@ def k_means(X=None, init_c=None, n_iters=50):
     if X is None:
         X, init_c = hw1_utils.load_data()
 
-    maxItr = 100
+    maxItr = 10
     c = init_c
     K = 2
 
@@ -31,7 +31,7 @@ def k_means(X=None, init_c=None, n_iters=50):
     r = torch.zeros([K, T]) # keeps track of assignments
     d = torch.zeros([K, T]) # keeps track of distances of points from centroids
 
-    bPlot = True
+    bPlot = False
     i = 0       
     while i < maxItr:
 
@@ -43,6 +43,7 @@ def k_means(X=None, init_c=None, n_iters=50):
 
         
         # Assign points to clusters
+        r[r!=0] = 0 # Reset
         for n in range(T):
             ind = torch.argmin(d[:,n])
             for k in range(K):           
@@ -59,21 +60,14 @@ def k_means(X=None, init_c=None, n_iters=50):
         if bPlot:
             c1 = torch.reshape(c[:,0], (D,1))
             c2 = torch.reshape(c[:,1], (D,1))
-            r0 = r[0,:]
-            r1 = r[1,:]
-            ind0 = torch.argwhere(r0)
-            ind1 = torch.argwhere(r1)
-            x1 = X[:,ind0]
-            x2 = X[:,ind1]
+            x1 = X[:,torch.argwhere(r[0,:])]
+            x2 = X[:,torch.argwhere(r[1,:])]
             hw1_utils.vis_cluster(c1, x1, c2, x2)
-
-        # Reset
-        r[r!=0] = 0
 
         # Update loop index
         i += 1
 
-    return torch.t(c)
+    return c
 
 # main entrypoint
 centroids = k_means()
